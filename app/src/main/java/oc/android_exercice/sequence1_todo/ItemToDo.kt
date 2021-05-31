8410f1ba3d912a762ae654fc6137dcdcec322290
LaurineDargaud <dargaud.laurine@gmail.com>
2021-05-31 12:04:15 +0200

    fix serialization

--- app/src/main/java/oc/android_exercice/sequence1_todo/ItemToDo.kt
+++ app/src/main/java/oc/android_exercice/sequence1_todo/ItemToDo.kt
@@ -1,6 +1,5 @@
 package oc.android_exercice.sequence1_todo
 
-@Serializable
 class ItemToDo(var description: String = "", var fait: Boolean = false) {
 
 }
--- app/src/main/java/oc/android_exercice/sequence1_todo/ProfilListeToDo.kt
+++ app/src/main/java/oc/android_exercice/sequence1_todo/ProfilListeToDo.kt
@@ -1,6 +1,5 @@
 package oc.android_exercice.sequence1_todo
 
-@Serializable
 class ProfilListeToDo(var login:  String = "", var listes: MutableList<ListeToDo> = mutableListOf<ListeToDo>()) {
     fun ajouteListe(uneListe: ListeToDo) {
         listes.add(uneListe)
