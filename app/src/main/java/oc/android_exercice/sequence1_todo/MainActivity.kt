392a198dee7919d9e97a810772e0302c25aa87cb
mebazaap <pmebazaa@gmail.com>
2021-05-31 11:33:10 +0200

    V1 Pierre

--- app/src/main/java/oc/android_exercice/sequence1_todo/MainActivity.kt
+++ app/src/main/java/oc/android_exercice/sequence1_todo/MainActivity.kt
@@ -1,11 +1,81 @@
 package oc.android_exercice.sequence1_todo
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    //Initialisation des variables
+    private val PSEUDO: String = "Laurine et Pierre"
+    private var buttonOK: Button? = null;
+    private var pseudo: EditText? = null;
+    var sp: SharedPreferences? = null;
+    private var sp_editor: SharedPreferences.Editor? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sp = PreferenceManager.getDefaultSharedPreferences(this)
+        sp_editor = sp?.edit()
+        //Récupération des éléments graphiques du layout de l'activité dans le code
+        buttonOK = findViewById(R.id.buttonOK)
+        pseudo= findViewById(R.id.editTextPseudo)
+
+        //Appel à la méthode gérant les clicks sur le buttonOK
+        onClickFun()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val s: String? = sp?.getString("PSEUDO", "login inconnu")
+        pseudo?.setText(s)
+    }
+
+    private fun onClickFun() {
+        buttonOK!!.setOnClickListener {
+
+            val nom: String = pseudo?.text.toString()
+
+            // Stockage du pseudo pour une prochaine connexion
+            //var sp_editor: SharedPreferences.Editor? = sp?.edit()
+            if (sp_editor != null) {
+                sp_editor!!.putString("PSEUDO", nom)
+                sp_editor!!.commit()
+                // feature à tester
+                //SettingsActivity().edtPref.text=nom
+            }
+
+            // Lancement de l'activité ChoixListActivity en passant la valeur du pseudo
+            val intentVersChoixListActivity: Intent = Intent(this, ChoixListActivity::class.java).apply {
+                putExtra(PSEUDO, nom)
+            }
+            startActivity(intentVersChoixListActivity)
+        }
+    }
+
+    // affiche le menu ActionBar si la méthode renvoie vrai
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        // R.menu.menu dénote le fichier  res/menu/menu.xml
+        return true
+    }
+
+    // click sur un item du menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        when (id) {
+            R.id.menu_settings -> {
+                //Permet de lancer l'activité SettingsActivity
+                val intentVersSettingsActivity = Intent(this, SettingsActivity::class.java)
+                startActivity(intentVersSettingsActivity)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
